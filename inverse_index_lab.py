8404005bc57a72ac73dc913643263d4e727c79aa
vwvolodya <vwvolodya@yandex.ru>
2015-02-06 20:32:57 +0200

    done second task

--- inverse_index_lab.py
+++ inverse_index_lab.py
@@ -1,23 +1,22 @@
 # version code d345910f07ae
 coursera = 1
 # Please fill out this stencil and submit using the provided submission script.
-
-
-
-
-
 ## 1: (Task 1) Movie Review
 ## Task 1
+
+
 def movie_review(name):
+    from random import randint
     """
     Input: the name of a movie
     Output: a string (one of the review options), selected at random using randint
     """
-    return ...
-
-
+    variants = ["See it!", "A gem!", "Ideological claptrap!"]
+    return variants[randint(0, len(variants))]
 
 ## 2: (Task 2) Make Inverse Index
+
+
 def makeInverseIndex(strlist):
     """
     Input: a list of documents as strings
@@ -29,10 +28,19 @@ def makeInverseIndex(strlist):
     Feel free to use a loop instead of a comprehension.
 
     Example:
-    >>> makeInverseIndex(['hello world','hello','hello cat','hellolot of cats']) == {'hello': {0, 1, 2}, 'cat': {2}, 'of': {3}, 'world': {0}, 'cats': {3}, 'hellolot': {3}}
+    #>>> makeInverseIndex(['hello world','hello','hello cat','hellolot of cats']) == {'hello': {0, 1, 2}, 'cat': {2}, 'of': {3}, 'world': {0}, 'cats': {3}, 'hellolot': {3}}
     True
     """
-    pass
+    result = {}
+    for ind, el in enumerate(strlist):
+        for word in el.split():
+            if word in result.keys():
+                result[word] += [ind]
+            else:
+                result[word] = [ind]
+
+    r = {k: set(v) for k, v in result}
+    return r
 
 
 
@@ -43,13 +51,19 @@ def orSearch(inverseIndex, query):
     Output: the set of document ids that contain _any_ of the specified words
     Feel free to use a loop instead of a comprehension.
     
-    >>> idx = makeInverseIndex(['Johann Sebastian Bach', 'Johannes Brahms', 'Johann Strauss the Younger', 'Johann Strauss the Elder', ' Johann Christian Bach',  'Carl Philipp Emanuel Bach'])
-    >>> orSearch(idx, ['Bach','the'])
+    #>>> idx = makeInverseIndex(['Johann Sebastian Bach', 'Johannes Brahms', 'Johann Strauss the Younger', 'Johann Strauss the Elder', ' Johann Christian Bach',  'Carl Philipp Emanuel Bach'])
+    #>>> orSearch(idx, ['Bach','the'])
     {0, 2, 3, 4, 5}
-    >>> orSearch(idx, ['Johann', 'Carl'])
+    #>>> orSearch(idx, ['Johann', 'Carl'])
     {0, 2, 3, 4, 5}
     """
-    pass
+    result = set()
+    for word in query:
+        try:
+            result = result | inverseIndex[word]
+        except KeyError:
+            pass
+    return result
 
 
 
@@ -60,11 +74,17 @@ def andSearch(inverseIndex, query):
     Output: the set of all document ids that contain _all_ of the specified words
     Feel free to use a loop instead of a comprehension.
 
-    >>> idx = makeInverseIndex(['Johann Sebastian Bach', 'Johannes Brahms', 'Johann Strauss the Younger', 'Johann Strauss the Elder', ' Johann Christian Bach',  'Carl Philipp Emanuel Bach'])
-    >>> andSearch(idx, ['Johann', 'the'])
+    #>>> idx = makeInverseIndex(['Johann Sebastian Bach', 'Johannes Brahms', 'Johann Strauss the Younger', 'Johann Strauss the Elder', ' Johann Christian Bach',  'Carl Philipp Emanuel Bach'])
+    #>>> andSearch(idx, ['Johann', 'the'])
     {2, 3}
-    >>> andSearch(idx, ['Johann', 'Bach'])
+    #>>> andSearch(idx, ['Johann', 'Bach'])
     {0, 4}
     """
-    pass
+    result = set()
+    for word in query:
+        try:
+            result = result & inverseIndex[word]
+        except KeyError:
+            pass
+    return result
 
